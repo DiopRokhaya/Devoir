5a1a516a185983fee3b06eb85f47417a94d01c3d
DiopRokhaya <45308045+DiopRokhaya@users.noreply.github.com>
2019-02-16 11:52:47 +0200

    Add files via upload

---
+++ rokhy.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html>
+<meta charset="utf-8">
+<link href="style.css" rel="stylesheet" type="text/css" />
+<head>
+<body background=4b.jpg>
+<center>
+	<title>Contact</title>
+</head>
+<body>
+
+<body background=images-18.jpg> 
+<big> <big> <big>
+<TABLE cellspacing=0 BORDER=4%>
+<TR> 
+<TD COLSPAN=2><center><img src= "ROKHAYA.jpg"width=200 height=150 alt= "MOI"></TD>
+</TR>
+<TR>
+<TD><?php echo "Nom";?>:</TD>
+<TD>
+Diop
+</TD>
+</TR>
+
+<TR>
+<TD><?php echo "Prénom";?>:</TD>
+<TD>
+Rokhaya
+</TD>
+</TR>
+<TR>
+<TD><?php echo "Date de naissance";?>:</TD>
+<TD>
+24/05/1995
+</TD>
+</TR>
+<TR>
+<TD><?php echo "Identifiant slack";?>:</TD>
+<TD>
+Rokhaya_Diop
+</TD>
+</TR>
+<TR>
+<TD><?php echo "Numéro de téléphone";?>:</TD>
+<TD>
+779737992
+</TD>
+</TR>
+<TR>
+<br><br>
+<TD><?php echo "Email";?>:</TD>
+</br></br>
+<TD>
+diop98018@gmail.com
+</TD>
+</TR>
+<TR>
+<TD><?php echo "Matricule";?>:</TD>
+<TD>
+201708DEQ
+</TD>
+</TR>
+<TR>
+<TD><?php echo "Adresse";?>:</TD>
+<TD>
+HLM5
+</TD>
+</TR>
+<TR>
+<TD><?php echo "Votre sexe"; ?>:</TD>
+<TD>
+Homme : <INPUT type=radio name="Votre sexe" value="M">
+<br>Femme : <INPUT type=radio name="Votre sexe" value="F">
+</TD>
+</TR>
+<TR>
+<TD><br/> <?php echo "Niveau d'étude"; ?>:</TD>
+<TD>
+<SELECT name="Votre niveau d'étude:">
+<OPTION VALUE="licence1">licence1</OPTION>
+<OPTION VALUE="licence2">licence2</OPTION>
+<OPTION VALUE="licence3">licence3</OPTION>
+<OPTION VALUE="Master1">Master1</OPTION>
+<OPTION VALUE="Master2">Master2</OPTION>
+<OPTION VALUE="Doctorat">Doctorat</OPTION>
+</SELECT>
+</TD>
+</TR>
+<TR>
+<TD> <?php echo "Numéro carte d'étudiant";?>:</TD>
+<TD>
+<TEXTAREA rows="3" name="Numéro carte d'étudiant">
+Numéro carte d'étudiant</TEXTAREA>
+</TD>
+</TR>
+<TR> 
+<TD COLSPAN=2><INPUT type="submit" value="Envoyer"></INPUT></TD>
+</TR>
+</TABLE>
+
+</big>
+</body>
+</body>
+</html>
